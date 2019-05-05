3f2ae3c66855bc078c2f9f4a6dffde9e4ab4f767
huzzzz <huzefachasmai@gmail.com>
2019-05-05 14:57:58 +0530

    Ran exp on Phantom

--- code/myMainScript.m
+++ code/myMainScript.m
@@ -16,8 +16,9 @@ is_color = 1;
 tic;
 %% Load Image to be reconstructed
 % Loading the denoised image
-% original_image = phantom(128);
-original_image = im2double(imread('../data/brain_mri.jpg'));
+original_image = phantom(128);
+
+% original_image = im2double(imread('../data/brain_mri.jpg'));
 
 savefig(my_color_scale,original_image,"Original Image","Original.png",1,to_save);
 [h,w] = size(original_image);
@@ -33,10 +34,10 @@ stop_ang  = 180;
 del_ang   = (stop_ang - start_ang)/num_views;
 del_t     = h/num_bins;
 lambda    = 1;
-n_iter    = 100;
-variant   = 'Additive';
+n_iter    = 200;
+% variant   = 'Additive';
 % variant   = 'Multiplicative';
-% variant   = 'SIRT';
+variant   = 'SIRT';
 
 %% Construct 'b' the radon transform
 radon_transform = constructRadonTransform(original_image, num_bins, num_views, start_ang, stop_ang, del_ang, del_t);
@@ -85,7 +86,6 @@ function savefig(my_color_scale,modified_pic,title_name,file_name,is_color,to_sa
 	
 	imagesc(modified_pic), title(title_name), colorbar, daspect([1 1 1]), axis tight;
 	impixelinfo();
-	
 	if to_save == 1
 		saveas(fig,file_name);
 		% close(fig);
